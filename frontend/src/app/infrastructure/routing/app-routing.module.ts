0a080c296cc5402a7d0609c087e8a37d5cb64f24
stamenkovicx <natasastamenkovic99@gmail.com>
2025-10-14 18:35:59 +0200

    established frontend

---
+++ frontend/src/app/infrastructure/routing/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from 'src/app/feature-modules/layout/home/home.component';
+import { LoginComponent } from '../auth/login/login.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { RegistrationComponent } from '../auth/registration/registration.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegistrationComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
